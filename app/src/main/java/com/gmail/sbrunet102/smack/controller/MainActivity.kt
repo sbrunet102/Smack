7e0d5635e0eec2e799c6359fc3839d1d3b5a9adc
Sylvain Brunet <sbrunet102@gmail.com>
2020-06-27 09:19:03 -0400

    lesson 20

--- app/src/main/java/com/gmail/sbrunet102/smack/controller/MainActivity.kt
+++ app/src/main/java/com/gmail/sbrunet102/smack/controller/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.navigation.NavigationView
@@ -28,6 +29,7 @@ import com.gmail.sbrunet102.smack.utilities.BROADCAST_USER_DATA_CHANGE
 import com.gmail.sbrunet102.smack.utilities.SOCKET_URL
 import io.socket.client.IO
 import io.socket.emitter.Emitter
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,6 +37,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     val socket = IO.socket(SOCKET_URL)
+    lateinit var channelAdapter: ArrayAdapter<Channel>
+
+    private fun setupAdapters(){
+        channelAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,MessageService.channels)
+        channel_list.adapter = channelAdapter
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +72,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
 //        navView.setupWithNavController(navController)
+
+        setupAdapters()
     }
 
     override fun onResume() {
@@ -82,7 +92,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val userDataChangeReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
+        override fun onReceive(context: Context, intent: Intent?) {
             if (AuthService.isLoggedIn) {
                 userNameNavHeader.text = UserDataService.name
                 userEmailNavHeader.text = UserDataService.email
@@ -95,6 +105,11 @@ class MainActivity : AppCompatActivity() {
                     )
                 )
                 loginBtnNavHeader.text = "Logout"
+                MessageService.getChannels(context){complete->
+                    if(complete){
+                        channelAdapter.notifyDataSetChanged()
+                    }
+                }
             }
         }
     }
@@ -158,9 +173,7 @@ class MainActivity : AppCompatActivity() {
 
             val newChannel = Channel(channelName, channelDescription, channelId)
             MessageService.channels.add(newChannel)
-            println(newChannel.name)
-            println(newChannel.description)
-            println(newChannel.id)
+            channelAdapter.notifyDataSetChanged()
         }
     }
 
--- app/src/main/java/com/gmail/sbrunet102/smack/services/MessageService.kt
+++ app/src/main/java/com/gmail/sbrunet102/smack/services/MessageService.kt
@@ -1,8 +1,53 @@
 package com.gmail.sbrunet102.smack.services
 
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.gmail.sbrunet102.smack.model.Channel
+import com.gmail.sbrunet102.smack.utilities.URL_GET_CHANNELS
+import org.json.JSONException
 
 object MessageService {
 
     val channels = ArrayList<Channel>()
+
+    fun getChannels(context:Context,complete:(Boolean)->Unit){
+
+        val channelRequest = object :  JsonArrayRequest(Method.GET, URL_GET_CHANNELS,null,Response.Listener { response ->
+
+            try {
+
+                for(x in 0 until response.length()){
+                    val channel = response.getJSONObject(x)
+                    val channelName = channel.getString("name")
+                    val channelDesc = channel.getString("description")
+                    val channelId = channel.getString("_id")
+
+                    val newChannel = Channel(channelName,channelDesc,channelId)
+                    this.channels.add(newChannel)
+                }
+                complete(true)
+            }catch (e:JSONException){
+                Log.d("JSON","EXC"+e.localizedMessage)
+                complete(false)
+            }
+
+        },Response.ErrorListener {error ->
+            Log.d("ERROR","Could not retreive channels")
+            complete(false)
+        }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer ${AuthService.authToken}")
+                return headers
+             }
+        }
+        Volley.newRequestQueue(context).add(channelRequest)
+    }
 }
